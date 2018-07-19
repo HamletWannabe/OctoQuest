ff36d981c0d59e500cd05f2b60ddebd4f09d228f
willstansbury <40803196+willstansbury@users.noreply.github.com>
2018-07-19 15:00:29 -0700

    delta rpm?

--- install.sh
+++ install.sh
@@ -7,6 +7,9 @@ yum -y update
 # install general libraries like Java or ImageMagick
 yum -y install default-jre ImageMagick
 
+#WILLIAM PUT THIS IN
+yum install deltarpm
+
 # add nodejs to yum
 curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
 yum -y install nodejs #default-jre ImageMagick
